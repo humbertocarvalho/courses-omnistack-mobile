91147f6a4e4197a3e6120850ae1a90b9b90f458a
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-11 16:45:54 -0300

    Login page.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,67 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import {
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+//import logo from '../assets/logo.png';
 
 // import { Container } from './styles';
 
 export default function Login() {
-  return <View style={style.container} />;
+  return (
+    <KeyboardAvoidingView
+      behavior='padding'
+      enabled={Platform.OS === 'ios'}
+      style={styles.container}
+    >
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect={false}
+        placeholder='Digite seu usuário no Github'
+        placeholderTextColor='#999'
+        style={styles.input}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30
+  },
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15
+  },
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#DF4723',
+    borderRadius: 4,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16
+  }
 });
